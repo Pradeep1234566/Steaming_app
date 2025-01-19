54ba48e802bfc32ed50ba2699419a3ca604bdc34
Pradeep <pawarpradeep1707@gmail.com>
2025-01-19 16:53:47 +0530

    Serach Box done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,9 +3,73 @@ import 'package:flutter/material.dart';
 class MyHome extends StatelessWidget {
   const MyHome({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor:
+          Colors.black, // Set the background color to match the design
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Top Section
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.person, color: Colors.white, size: 28),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 80.0, bottom: 15),
+                    child: Image.asset(
+                      'asset/images/Icon.png', // Replace with your actual logo asset path
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Search Bar
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: "Search your hero...",
+                          hintStyle: TextStyle(color: Colors.grey[400]),
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16.0), // Padding added here
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    const Icon(Icons.search, color: Colors.white),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ),
+            ),
+            //Learn your favourite Hero
+            Text(
+              "Learn Your",
+              style: TextStyle(
+                  fontSize: 25, color: Colors.white, fontFamily: 'Roboto'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
